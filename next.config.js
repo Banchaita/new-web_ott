72f6a530d2ae516fda5be6c8fc37aa7bc1653bba
Banchaita <biswassanchaita1998@gmail.com>
2023-10-29 21:50:46 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  // useFileSystemPublicRoutes: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
